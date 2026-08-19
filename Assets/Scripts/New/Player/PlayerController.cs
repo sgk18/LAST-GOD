fb37072587c535d6d52065929fa644eff754f47f
aatndev <ksaadithyan@gmail.com>
2026-08-19 18:03:34 +0530

    Added dash mechanic;

--- Assets/Scripts/New/Player/PlayerController.cs
+++ Assets/Scripts/New/Player/PlayerController.cs
@@ -1,18 +1,34 @@
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movement")]
     public float moveSpeed = 7;
-    public float jumpForce = 10;
-    public float gravity = 30;
 
+    [Header("Jump N Dash")]
+    public float jumpForce = 10;
+    public float dashSpeed = 25;
+    public float dashTime = 0.2f;
+    public float dashCoolDown = 1;
+    private float dashSide = 1;
+    private bool isDashing;
+    private bool canDash = true;
+
+    [Header("Phy")]
+    public float gravity = 30;
+    private float g;
     private CharacterController characterController;
-    private float moveX;
     private float yVel;
+
+    [Header("input")]
+    private float moveX;
 
     void Start()
     {
+        g = gravity;
         characterController = GetComponent<CharacterController>();
     }
     void OnEnable()
@@ -26,11 +42,28 @@ public class PlayerController : MonoBehaviour
             yVel = -1;
         }
 
-        yVel -= gravity * Time.deltaTime;
+        yVel -= g * Time.deltaTime;
+
+        if(moveX < 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        else if(moveX > 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
 
         Vector3 movement = new Vector3(moveX * moveSpeed, yVel, 0);
 
-        characterController.Move(movement * Time.deltaTime);
+        if (isDashing)
+        {
+            Debug.Log("Dashing...");
+            characterController.Move(dashSpeed * transform.right * Time.deltaTime);
+        }
+        else
+        {
+            characterController.Move(movement * Time.deltaTime);
+        }
     }
 
     //input
@@ -44,5 +77,34 @@ public class PlayerController : MonoBehaviour
         {
             yVel = jumpForce;
         }
+    }
+
+    void OnDash()
+    {
+        Debug.Log("Dash key pressed");
+        if (canDash)
+        {
+            Debug.Log("Can dash");
+            StartCoroutine(Dash());
+        }
+        else
+        {
+            Debug.Log("Cant dash");
+        }
+    }
+    IEnumerator Dash()
+    {
+        isDashing = true;
+        g = 0;
+        StartCoroutine(DashCoolDown());
+        yield return new WaitForSeconds(dashTime);
+        g = gravity;
+        isDashing = false;
+    }
+    IEnumerator DashCoolDown()
+    {
+        canDash = false;
+        yield return new WaitForSeconds(dashCoolDown);
+        canDash = true;
     }
 }

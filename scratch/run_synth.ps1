1c158e86ee1436245f2fc27025a2193ef79fc575
Suryachalam V M <suryachalam18@gmail.com>
2026-09-10 21:25:18 +0530

    feat: implement Aeron first-person player controller, movement logic, and character assets

--- scratch/run_synth.ps1
+++
@@ -1,8 +0,0 @@
-
-Add-Type -AssemblyName System.Speech
-$synth = New-Object System.Speech.Synthesis.SpeechSynthesizer
-$synth.Rate = -2
-$synth.Volume = 100
-$synth.SetOutputToWaveFile('c:/projects/LAST-GOD/scratch/raw_voice.wav')
-$synth.Speak('Wake up, Aeron... Awaken your power.')
-$synth.Dispose()
